12854d3c147dfe0e7200fb38e84cc17ba1551c23
ANKIT <itsankitagarwal000@gmail.com>
2023-11-21 09:14:48 +0530

    used Link

--- app/aboutus/page.js
+++ app/aboutus/page.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import Link from 'next/link'
+import React, { Fragment } from 'react'
 
 function AboutUs() {
   return (
+    <Fragment>
     <h1>About us</h1>
+
+    <ul>
+        <li>
+            <Link href="/aboutus/1">YASH</Link>
+        </li>
+        <li>
+            <Link href="/aboutus/2">VAIBHAV</Link>
+        </li>
+        <li>
+            <Link href="/aboutus/3">SURESH</Link>
+        </li>
+    </ul>
+    </Fragment>
   )
 }
 
